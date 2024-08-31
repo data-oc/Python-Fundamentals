442a9167ee3f32dc936233a56dd01c367c474f1f
Apiwat Tatsanakitti <apiwat.tatsanakitti@gmail.com>
2024-08-31 20:36:10 +0700

    Delete Part_3 directory

--- Part_3/condition/2_nested_if.py
+++
@@ -1,7 +0,0 @@
-x = 25
-if x > 10:
-    print("x greater than 10")
-    if x > 20:
-        print("x greater than 20")
-    else:
-        print("x less than 20")
--- Part_3/loop/1_while_loop.py
+++
@@ -1,6 +0,0 @@
-input_number = int(input("Enter number: "))
-
-i = 1
-while i <= input_number:
-    print(i)
-    i += 1
