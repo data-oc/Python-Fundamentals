6d39667c465d2caa86c33564161915f6bf193c80
Apiwat Tatsanakitti <apiwat.tatsanakitti@gmail.com>
2024-10-12 15:37:21 +0700

    Update example_5.py

--- part_5/exception_handling/example_5.py
+++ part_5/exception_handling/example_5.py
@@ -1,4 +1,13 @@
-# If we use "raise exception" we cannot use "try" and "except".
+"""
+If we use "raise Exception," we can still use "try" and "except." 
+
+Example:
+try:
+    # Some code that may raise an exception
+    raise Exception("An error occurred")
+except Exception as e:
+    print(f"Caught an exception: {e}")
+"""
 
 my_input = int(input("Your input: "))
 
@@ -6,4 +15,3 @@ if my_input < 1 or my_input > 100:
     raise Exception("Your input must has number from 1 to 100.")
 else:
     print(my_input)
-
